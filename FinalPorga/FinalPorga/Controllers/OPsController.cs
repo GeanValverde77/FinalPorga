e365c2f413def2c1be85a5fe40a12bb74828fbcc
GEAN MARCO VALVERDE ARANA <a2019114321@estudiantes.upsa.edu.bo>
2020-12-11 21:29:28 -0400

    Finalizado

---
+++ FinalPorga/FinalPorga/Controllers/OPsController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FinalPorga.Models;
+
+namespace FinalPorga.Controllers
+{
+    public class OPsController : ApiController
+    {
+        private DataContext db = new DataContext();
+
+        // GET: api/OPs
+        public IQueryable<OP> GetOPs()
+        {
+            return db.OPs;
+        }
+
+        //Funcion
+        [HttpGet]
+        [Route("{numero:int}")]
+        public string Operacion(int numero)
+        {
+            if (numero < 0)
+                return "ERROR";
+            if (numero == 0)
+                return "Realizado por Gean Valverde";
+            return "Usted ingreso el numero " + numero.ToString();
+        }
+
+        // GET: api/OPs/5
+        [ResponseType(typeof(OP))]
+        public IHttpActionResult GetOP(int id)
+        {
+            OP oP = db.OPs.Find(id);
+            if (oP == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(oP);
+        }
+
+        // PUT: api/OPs/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutOP(int id, OP oP)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != oP.numero)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(oP).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OPExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/OPs
+        [ResponseType(typeof(OP))]
+        public IHttpActionResult PostOP(OP oP)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.OPs.Add(oP);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = oP.numero }, oP);
+        }
+
+        // DELETE: api/OPs/5
+        [ResponseType(typeof(OP))]
+        public IHttpActionResult DeleteOP(int id)
+        {
+            OP oP = db.OPs.Find(id);
+            if (oP == null)
+            {
+                return NotFound();
+            }
+
+            db.OPs.Remove(oP);
+            db.SaveChanges();
+
+            return Ok(oP);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool OPExists(int id)
+        {
+            return db.OPs.Count(e => e.numero == id) > 0;
+        }
+    }
+}
---
+++ FinalPorga/FinalPorga/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FinalPorga.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<FinalPorga.Models.OP> OPs { get; set; }
+    }
+}
